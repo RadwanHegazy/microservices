66e1fa68d8f1b5120fe9ca34566cd885c11c2a83
RadwanHegazy <radwangaber22@gmail.com>
2024-10-16 16:41:40 +0300

    update get all todos endpoint

--- api_gateway/core/app/apis/views/get.py
+++ api_gateway/core/app/apis/views/get.py
@@ -5,17 +5,9 @@ from rest_framework.response import Response
 
 class GetTodos(APIView) : 
 
-    def dispatch(self, request, *args, **kwargs):
-        response = super().dispatch(request, *args, **kwargs)
-        response['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
-        response['Pragma'] = 'no-cache'
-        response['Expires'] = '0'
-        return response
-
     def get(self, request) : 
         url = FLASK + "v1/get/"
         req = requests.get(url)
-        print(req.url)
         return Response(req.json(), status=req.status_code)
     
 
