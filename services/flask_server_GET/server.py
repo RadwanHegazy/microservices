9949943cb0e3c6438811035b3ea2ef8e345d65da
RadwanHegazy <radwangaber22@gmail.com>
2024-10-16 14:13:45 +0300

    fix get all todos issue

--- services/flask_server_GET/server.py
+++ services/flask_server_GET/server.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify
-from db import todo
+from db import db, TodoTable
 
 app = Flask(__name__)
 
 @app.get("/v1/get/")
 def get_all_data() : 
+    db.create_table(TodoTable)
+    todo = db.manage(TodoTable)
     return jsonify(todo.all()), 200
 
 if __name__ == "__main__" : 
